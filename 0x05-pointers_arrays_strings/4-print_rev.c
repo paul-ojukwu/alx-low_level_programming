ad16b297c1b9c06779a8547958160025a3001f20
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-17 08:58:19 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev-prints a string in reverse order
+ * _putchar-prints to stdout
+ * @s: strimg to be printed in reverse order
+ */
+void print_rev(char *s)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		counter++;
+	}
+	for (i = counter; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
