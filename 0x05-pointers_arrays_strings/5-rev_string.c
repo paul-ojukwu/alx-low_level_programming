55c4cc0548c905d88a5a76d68714856391652b78
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-17 14:36:58 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string-prints a string in reverse order
+ * _putchar-prints to stdout
+ * @s: string to be printed in reverse order
+ */
+void rev_string(char *s)
+{
+	int i;
+	int counter = 0;
+	int j;
+	char *start_ptr;
+	char *end_ptr;
+	char ch;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		counter++;
+	}
+	j = counter - 1;
+	start_ptr = s;
+	end_ptr = s + j;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		ch = *end_ptr;
+		*end_ptr = *start_ptr;
+		*start_ptr = ch;
+
+		start_ptr++;
+		end_ptr--;
+	}
+}
