380fa61932e7378b0a87b9cc8079300bf5e01abf
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-24 02:35:23 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int cmp = 0;
+	int cmp;
 
 	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
 	{
-		cmp += s1[i] - s2[i];
+		cmp = s1[i] - s2[i];
 		if (s1[i] != s2[i])
 		{
 			break;
 		}
 	}
+	i++;
 	return (cmp);
 }
