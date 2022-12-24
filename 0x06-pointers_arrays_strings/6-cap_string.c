407369de02fb8978103b32705ca0cc1e461bf79b
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-24 05:46:33 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,20 @@
  */
 char *cap_string(char *s)
 {
-
-	int i = 0;
-
-	while (s[i])
+	int i;
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+	}
+	for (i = 1; s[i] != '\0'; i++)
 	{
 		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
 		{
-			s[i + 1] -= 32;
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] -= 32;
+			}
 		}
-		i++;
 	}
 	return (s);
 }
