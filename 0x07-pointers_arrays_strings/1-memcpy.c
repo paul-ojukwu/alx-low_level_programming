a52d67e698df198789cc0ac36bf481d448dade28
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-28 10:30:13 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,9 +7,9 @@
  * @n: number of bytes to be copied
  * Return: pointer to the modified string
  */
-char *_memset(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
 	while (i < n)
