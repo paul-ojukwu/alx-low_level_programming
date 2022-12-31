7f80096d0b042f116bd8e3d451f31015a5ca3817
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-31 13:42:18 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    char *s = "hellu, world";
-    char *f = "leho";
-    unsigned int n;
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-    n = _strspn(s, f);
-    printf("%u\n", n);
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
     return (0);
 }
