c1754fd138024032324f1ab7bc10ba14879b1e89
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-03 01:38:09 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
-int _sqrt_recursion(int n);
 
 /**
- * _sqrt_recursion-returns the natural square root of a number
- * @n: integer
+ * _divide-returns square root of a number
+ * @x: integer
+ * @y: integer
  * Return: square root of n
  */
 int _divide(int x, int y)
@@ -16,6 +16,11 @@ int _divide(int x, int y)
 		return (y);
 	return (_divide(x, y + 1));
 }
+/**
+ * _sqrt_recursion-returnz natural square root of a number
+ * @n: integer
+ * Return: natural square of n
+ */
 int _sqrt_recursion(int n)
 {
 	return (_divide(n, 1));
