13b25b8431d0b82c5025e3b4f5f752c545c35f2c
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-05 13:23:27 -0800

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-prints number of coins for change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: integer showing number of coins
+ */
+int main(int argc, char *argv[])
+{
+	int cent = atoi(argv[1]);
+	int coins[5] = {25, 10, 5, 2, 1};
+	int i, ans, rem;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = 0;
+	while (coins[i])
+	{
+		if (cent / coins[i] >= 1)
+		{
+			ans = cent / coins[i];
+			if (cent % coins[i] == 0)
+			{
+				printf("%d\n", ans);
+				return (0);
+			}
+			else
+			{
+				rem = cent % coins[i];
+				break;
+			}
+		}
+		i++;
+	}
+	for (; coins[i]; i++)
+	{
+		if (rem / coins[i] >= 1)
+		{
+			ans += rem / coins[i];
+			rem = rem % coins[i];
+		}
+	}
+	printf("%d\n", ans);
+	return (0);
+}
