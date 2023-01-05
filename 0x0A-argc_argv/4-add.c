6bc22a7fe1c7b5d99a1bee8d3eefba25275554f9
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-05 11:11:20 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-
+int isNumber(char *s);
 /**
  * main-prints the sum of positive numbers
  * @argc: argument count
@@ -19,13 +19,29 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(atoi(argv[i])))
+		if (!isNumber(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
-		}	
+		}
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
+}
+/**
+ * isNumber-checks if a character string contains numbers
+ * @s: pointer to the character string
+ * Return: 1 or 0
+ */
+int isNumber(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (!isdigit(s[i]))
+			return (0);
+	}
+	return (1);
 }
