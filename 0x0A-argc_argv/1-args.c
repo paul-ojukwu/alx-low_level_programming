bb0e383245e442d9c493cb29fac5ad40b8b3a8e0
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-05 04:08:15 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,8 @@
 
 /**
  * main-prints the number of arguments
- * @argc: argument vector
+ * @argv: argument vector
+ * @argc: argument count
  * Return: always 0
  */
 int main(int argc, char *argv[] __attribute__((unused)))
