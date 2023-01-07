6282861282823a0591e6dd51b50592cf1dc5061f
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-07 14:29:52 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
+
 	if (size == 0)
 		return ('\0');
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return ('\0');
-	ptr[0] = c;
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
