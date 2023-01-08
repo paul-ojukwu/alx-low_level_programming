22841067f38df3c94223a13bf08acac8c95f9cfc
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-08 15:21:19 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int length1 = 0, length2 = 0, i;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 	{
 		length1 += 1;
