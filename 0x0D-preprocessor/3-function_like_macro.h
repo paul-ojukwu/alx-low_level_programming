c96321fd5ad2e962f7a9981800313bf91f95d1cd
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-15 14:04:03 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < 0 ? (x * (-1)) : x > 0 ? (x) : 0)
 
-#endif /*FUNCTION_LIKE_MACRO*/
+#endif /*FUNCTION_LIKE_MACRO_H*/
