7e5bb353c7df94fadddb0347e5ce5ec32bd32ce1
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-15 11:40:16 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,15 @@
 #include <unistd.h>
+void _puts(char *str);
 int _putchar(char c);
 /**
  * main-prints the name of the file it was compiled from
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-	_putchar(__NAME__);
+	char *s = __FILE__;
+
+	_puts(s);
 	return (0);
 }
 /**
@@ -19,4 +22,21 @@ int main (void)
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+/**
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
 }
