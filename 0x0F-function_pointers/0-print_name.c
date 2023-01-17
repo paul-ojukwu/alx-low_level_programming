8d40cc363fec03c801f096aac74d221e1d53551e
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-17 12:07:47 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name-prints a name
+ * @name: pointer to the name
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
