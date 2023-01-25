38e76d4575921dc46f47fcfb9e674e47be30238a
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-25 12:42:04 -0800

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * _strlen-returns the length of a string
+ * @s: pointer to the string
+ * Return: an integer
+ */
+int _strlen(char *s)
+{
+	int i, sum = 0;
+
+	for (i = 0; s[i]; i++)
+		sum += 1;
+	return (sum);
+}
+/**
+ * print_list-prints all the elements of a list_t list
+ * @h: pointer to the head of a singly linked list
+ * Return: an unsigned int
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+	const list_t *ptr;
+
+	ptr = h;
+	while (ptr->next)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", _strlen(ptr->str), ptr->str);
+		ptr = ptr->next;
+		counter++;
+	}
+	if (ptr->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", _strlen(ptr->str), ptr->str);
+	counter++;
+	return (counter);
+}
